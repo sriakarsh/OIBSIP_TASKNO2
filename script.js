82162021722bd03183b53a7cf5f297279c8d1454
sriakarsh <81280908+sriakarsh@users.noreply.github.com>
2022-12-05 20:41:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById('menu');
+let nav = document.querySelector('.nav-links');
+
+menu.addEventListener('click', () => {
+    nav.classList.toggle('slide-into');
+
+});
